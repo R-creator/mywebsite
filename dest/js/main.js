537675700c15195338c159c3f08bb452d0249bb5
Rollin Dickerson <rollind5000@gmail.com>
2019-07-21 15:34:10 -0400

    adding JS changes

--- dest/js/main.js
+++ dest/js/main.js
@@ -11,12 +11,12 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
+    if (!showMenu) {
        menuBtn.classList.add('close');
        menu.classList.add('show');
        menuNav.classList.add('show');
        menuBranding.classList.add('show');
-       navItems.forEach( item=> item.classList.add('show'));
+       navItems.forEach(item => item.classList.add('show'));
 
        // Set Menu State
        showMenu = true;
@@ -25,7 +25,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach( item=> item.classList.remove('show'));
+        navItems.forEach(item=> item.classList.remove('show'));
  
         // Set Menu State
         showMenu = false;
