012584cc0bc3bdddd20671f60983bd1ca30ac97b
Rollin Dickerson <rollind5000@gmail.com>
2019-07-21 21:53:49 -0400

    Modified JS

--- dest/js/main.js
+++ dest/js/main.js
@@ -20,12 +20,12 @@ function toggleMenu() {
 
        // Set Menu State
        showMenu = true;
-    } else{
+    } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach(item=> item.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
  
         // Set Menu State
         showMenu = false;
